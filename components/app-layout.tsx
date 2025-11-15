4c66a76599c9955eccf80db578735561d8dfc617
UtachiCodes <abdoullahaljersi@gmail.com>
2025-11-15 14:06:46 +0000

    test

--- components/app-layout.tsx
+++ components/app-layout.tsx
@@ -9,7 +9,7 @@ import { StatusBar } from "@/components/status-bar"
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen w-full overflow-x-hidden">
+      <div className="flex min-h-screen w-full">
         <AppSidebar />
         <div className="flex-1 flex flex-col overflow-hidden relative">
           {/* Ambient background layer */}
@@ -18,12 +18,10 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
             className="pointer-events-none absolute inset-0 -z-10 bg-[radial-gradient(1200px_600px_at_80%_-10%,_rgba(255,255,255,0.06),_transparent_70%),radial-gradient(900px_500px_at_0%_100%,_rgba(255,255,255,0.04),_transparent_70%)]"
           />
           <StatusBar />
-          <main className="flex-1 overflow-auto relative w-full">
+          <main className="flex-1 overflow-auto relative">
             {/* Vignette edge fade for cinematic look */}
             <div aria-hidden className="pointer-events-none absolute inset-0 -z-10 bg-[radial-gradient(80%_80%_at_50%_0%,_transparent_40%,_rgba(0,0,0,0.25)_100%)]" />
-            <div className="relative mx-auto flex min-h-full w-full max-w-[1600px] flex-col px-4 py-4 sm:px-6 lg:px-10">
-              {children}
-            </div>
+            {children}
           </main>
         </div>
       </div>
